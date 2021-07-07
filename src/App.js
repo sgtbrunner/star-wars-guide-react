2efc469342452b76b0fa240d3d9c75146364cd27
sgtbrunner <guilherme.brunner@gmail.com>
2021-07-07 08:30:01 -0300

    automatically lints and formats files

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       characters: [],
       searchfield: '',
@@ -20,30 +20,33 @@ class App extends Component {
       race: '',
       planet: '',
       movies: '',
-      showmodal: false
-    }
-    console.log(props)
+      showmodal: false,
+    };
+    console.log(props);
   }
 
   componentDidMount() {
     // this.props.getCharacters();
-    createList().then(response => this.setState({characters: response}));
+    createList().then((response) => this.setState({ characters: response }));
   }
 
   onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value});
-  }
+    this.setState({ searchfield: event.target.value });
+  };
 
   openModal = (event) => {
-    this.setState({clickedcard: this.state.characters[event.target.id]})
-    getStats(this.state.characters[event.target.id].species)
-    .then(response => this.setState({race: response}));
-    getStats(this.state.characters[event.target.id].homeworld)
-    .then(response => this.setState({planet: response}));
-    getFilms(this.state.characters[event.target.id].films)
-    .then(response => this.setState({movies: response}));
-    this.setState({showmodal: true});
-  }
+    this.setState({ clickedcard: this.state.characters[event.target.id] });
+    getStats(this.state.characters[event.target.id].species).then((response) =>
+      this.setState({ race: response })
+    );
+    getStats(this.state.characters[event.target.id].homeworld).then((response) =>
+      this.setState({ planet: response })
+    );
+    getFilms(this.state.characters[event.target.id].films).then((response) =>
+      this.setState({ movies: response })
+    );
+    this.setState({ showmodal: true });
+  };
 
   onCloseClick = () => {
     this.setState({
@@ -51,54 +54,53 @@ class App extends Component {
       race: '',
       planet: '',
       movies: '',
-      showmodal: false
+      showmodal: false,
     });
-  }
+  };
 
   render() {
-    const filteredCharacters = this.state.characters.filter(character => {
-        return character.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-      } 
-    );
+    const filteredCharacters = this.state.characters.filter((character) => {
+      return character.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    });
 
     if (!this.state.characters.length) {
-        return (
-            <div id="temp-page">
-              <div className ="page-loader animate-flicker">Please wait...</div>
-            </div>      
-      )} else {
-            return (
-              <div>
-                <Header />
-                <div id="flex-container">
-                  <SearchBox searchChange={this.onSearchChange}/>
-                  <CardList 
-                      characters = {this.state.characters}
-                      filteredCharacters = {filteredCharacters}
-                      openModal = {this.openModal}
-                  />
-                  <Modal 
-                    showModal = {this.state.showmodal} 
-                    character = {this.state.clickedcard}
-                    race = {this.state.race}
-                    planet = {this.state.planet}
-                    movies = {this.state.movies}
-                    onCloseClick = {this.onCloseClick}
-                  />
-                </div>
-                <Footer />
-              </div>
-            )        
-        }
+      return (
+        <div id="temp-page">
+          <div className="page-loader animate-flicker">Please wait...</div>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <Header />
+        <div id="flex-container">
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList
+            characters={this.state.characters}
+            filteredCharacters={filteredCharacters}
+            openModal={this.openModal}
+          />
+          <Modal
+            showModal={this.state.showmodal}
+            character={this.state.clickedcard}
+            race={this.state.race}
+            planet={this.state.planet}
+            movies={this.state.movies}
+            onCloseClick={this.onCloseClick}
+          />
+        </div>
+        <Footer />
+      </div>
+    );
   }
 }
 
-const maptDispatchToProps = dispatch => ({
-  getCharacters: () => dispatch(getCharacters())
+const maptDispatchToProps = (dispatch) => ({
+  getCharacters: () => dispatch(getCharacters()),
 });
 
-const mapStateToProps = state => ({
-  characters: state.characters.characters
+const mapStateToProps = (state) => ({
+  characters: state.characters.characters,
 });
 
 export default connect(mapStateToProps, maptDispatchToProps)(App);
